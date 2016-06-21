02edddfcc31d71d6e183e6959510e65d51c61ecd
Ben <Ben.Bahy@gmail.com>
2016-06-21 21:27:31 +0200

    route Vs controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,44 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect::to('Ben');
+});
+
+Route::get('Ben', function(){
+    return '<center> Ben <br> <img src="https://media.licdn.com/mpr/mpr/shrink_100_100/AAEAAQAAAAAAAAQJAAAAJDA1YTY1ZTM0LTkzMDMtNDJhNi1iYjU5LTI4NTE3ZTA1YmU4Zg.jpg"> </center>';
+});
+
+
+Route::get('user/{anything}', function($anything){
+    return 'you have entered in the uri pattern the argument : '.$anything;
+})->where('anything' , '[0-9]+');
+
+Route::get('shops', function(){
+	return 'show the shops a available';
+});
+
+Route::get('shops/create' , function(){
+	return 'add a shop to the database';
+});
+
+Route::post('shops/create' , function(){
+	$shopName = Input::get('shopName');
+	$shopDetails = Input::get('shopDetails');
+	//Validate the input and create a shop
+});
+
+Route::get('shops/{shopName}' , function($shopName){
+    return '<center><h1>this page for shop : </h1></center>'.$shopName ;
+});
+
+Route::get('shops/{shopName}/edit' , function($shopName){
+    return '<center><h1>return the form for editing the shop name </h1></center>'.$shopName ;
+});
+
+Route::patch('shops/{shopName}/edit' , function($shopName){
+    //getting details about the shop from database and update it
+});
+
+Route::delete('shops/{shopName}/delete' , function($shopName){
+    //deletes shop from database
 });
